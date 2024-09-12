b01465d31f50d7ac5492e6911b83f9a1426afdbe
Pratik Vishwakarma <103158578+Pratik0044@users.noreply.github.com>
2024-09-12 21:53:30 +0530

    Creating a new  Project Bloging App

---
+++ BlogingApp/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+
+const blog= require('./routes/blog')
+
+app.use(express.json())
+
+require('dotenv').config();
+const PORT = process.env.PORT||3000;
+
+const dbConnect = require("./config/database")
+dbConnect();
+
+app.use('api/v1',blog)
+
+app.listen(PORT,()=>{
+    console.log("Server start successfully ");
+    
+})
+
+app.get("/",(req,res)=>{
+    res.send(`<h1>HELLO JEE</h1>`)
+ })
