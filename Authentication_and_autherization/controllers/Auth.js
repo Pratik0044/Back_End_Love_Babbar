a06ac7be72cc896c537c39eb796f3994f51b686f
Pratik Vishwakarma <103158578+Pratik0044@users.noreply.github.com>
2024-10-31 08:57:11 +0530

    modify in auth.js controller

--- Authentication_and_autherization/controllers/Auth.js
+++ Authentication_and_autherization/controllers/Auth.js
@@ -1,3 +1,46 @@
 const bcrypt = require("bcrypt");
-const user = require("../models/user")
+const User = require("../models/user")
 
+exports.signup = async(req,res)=>{
+    try{
+        const {name,email,password,role} = req.body;
+        //if user already exist
+
+        const existingUser = await User.findOne({email});
+        if(existingUser) {
+            return res.status(400).json({
+                success:false,
+                message:"User already exist. Try with another Email"
+            });
+        }
+        //secure password
+
+        let hashedPassword;
+        try{
+        hashedPassword= await bcrypt.hash(password,10)
+        }
+        catch(er){
+            return res.status(500).json({
+                success:false,
+                message:"Error in hashing Password"
+            });
+        }
+
+        // create user
+
+        const user = await User.create({name,email,password:hashedPassword,role});
+
+        return res.status(200).json({
+            success:true,
+            message:"User created successfully",
+        })
+
+    }
+    catch(er){
+        console.error(er);
+        return res.status(500).json({
+            success:false,
+            message:"User can not be register, Please try again later",
+        })
+    }
+}
